9bd1b1a3efe8d9c2550f718087baf12cd6457bf2
Ecksi <jimecksi@gmail.com>
2019-05-26 21:05:21 -0600

    Move dependencies around

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const serveStatic = require("serve-static")
+const serveStatic = require('serve-static');
 const path = require('path');
 
-app = express();
+const app = express();
 
 app.use(serveStatic(path.join(__dirname, 'dist')));
 

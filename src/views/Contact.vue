b0f8778cb26bc9bd918643c93a505adb8c4dabb2
Ecksi <jimmy.dechant@gmail.com>
2019-05-26 18:35:26 -0600

    Add views

---
+++ src/views/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Suggestions? Contact us.</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Contact',
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/views/Players.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Meet the players</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Players',
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>

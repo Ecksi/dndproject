74e83cb079baaef1fcd0482d142726c4a565a1d0
Ecksi <jimecksi@gmail.com>
2019-05-31 06:45:06 -0600

    Add character portraits and display single character info

--- src/views/Players.vue
+++ src/views/Players.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
-    <h1>Meet the players</h1>
-    <div v-for="character in characters"
-      :key="generateKey(character)"
-    >
-      <CharacterSheet :character=character />
+    <h1 class="sub-header-text">Meet the players</h1>
+    <span class="center-text">Click on a player for more information</span>
+    <section class="char-portrait">
+      <img @click="selectedChar='Del'" src="../../public/assets/del.png" alt="del">
+      <img @click="selectedChar='Gardein Battlehammer'" src="../../public/assets/gardein.png" alt="gardein">
+      <img @click="selectedChar='Melon-Lord'" src="../../public/assets/melonlord.png" alt="melonlord">
+      <img @click="selectedChar='Tan-Tan'" src="../../public/assets/tantan.png" alt="tantan">
+      <img @click="selectedChar='Thistle'" src="../../public/assets/thistle.png" alt="thistle">
+    </section>
+    <div>
+      <div v-for="character in characters"
+        :key="generateKey(character)"
+        class="center-text"
+      >
+        <CharacterSheet :character=character :selectedChar="selectedChar" />
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +31,7 @@ export default {
   },
   data: () => ({
     characters: Characters,
+    selectedChar: ''
   }),
   methods: {
     generateKey(character) {
@@ -30,5 +42,14 @@ export default {
 </script>
 
 <style lang="less" scoped>
+  .char-portrait {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+    margin-top: 20px;
 
+    img:not(:last-child) {
+      margin-right: 30px;
+    }
+  }
 </style>

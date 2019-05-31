7b8e93c0da8a4b1147dc223c7cde4642d8d3e7c3
Ecksi <jimecksi@gmail.com>
2019-05-31 06:41:30 -0600

    Add App styling

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,24 @@ export default {
 
 
 <style lang="less">
-  * {
+  html, body {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
     font-family: Helvetica, sans-serif;
     font-size: 10px;
+  }
+
+  .center-text {
+    display: inherit;
+    font-size: 1.6rem;
+    text-align: center;
+  }
+
+  .sub-header-text {
+    font-size: 2.8rem;
+    margin-bottom: 30px;
+    margin-top: 20px;
+    text-align: center;
   }
 </style>

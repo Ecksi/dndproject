5452961b1f0972e2769f3c9ffa0764b30099c2f6
Ecksi <jimecksi@gmail.com>
2019-06-08 11:09:41 -0600

    Add hitPointBoxes molecule component

---
+++ src/components/molecules/characterSheet/HitPointBoxes.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hp-boxes">
+    <HitPointBox :hpBox="'top'" />
+    <HitPointBox :hpBox="'bot'" />
+  </div>
+</template>
+
+<script>
+  import HitPointBox from '@/components/atoms/characterSheet/HitPointBox';
+
+  export default {
+    name: 'HitPointBoxes',
+    components: {
+      HitPointBox
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .hp-boxes > div:last-child {
+    margin-top: 5px;
+    margin-bottom: 15px;
+  }
+</style>

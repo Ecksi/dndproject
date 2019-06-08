7a40f18541fa4fdff75ee012566523c21b8587c5
Ecksi <jimecksi@gmail.com>
2019-06-08 11:09:56 -0600

    Add coins molecule component

---
+++ src/components/molecules/characterSheet/Coins.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="coins">
+    <Coin>cp</Coin>
+    <Coin>sp</Coin>
+    <Coin>gp</Coin>
+    <Coin>ep</Coin>
+    <Coin>pp</Coin>
+  </div>
+</template>
+
+<script>
+  import Coin from '@/components/atoms/characterSheet/Coin';
+
+  export default {
+    name: 'coins',
+    components: {
+      Coin
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .coins > div:not(:last-child) {
+    margin-bottom: 5px;
+  }
+</style>

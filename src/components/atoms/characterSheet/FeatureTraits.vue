1396bb4b4000621cfa18cc07e65476edb3dadb6c
Ecksi <jimecksi@gmail.com>
2019-06-21 21:05:28 -0600

    Split featureTraits and Portrait into individual components

---
+++ src/components/atoms/characterSheet/FeatureTraits.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="round-box border">
+    <textarea name="" class="box-input"></textarea>
+    <label for="" class="box-text center-caps">
+      Features & Traits
+    </label>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FeatureTraits'
+  }
+</script>
+
+<style lang="less" scoped>
+  .round-box {
+    // background-color: white;
+    // opacity: 0.7;
+    height: 232px;
+    padding: 10px;
+    width: 236px;
+  }
+
+  .box-input {
+    background-color: transparent;
+    border: none;
+    padding: 5px;
+    height: 207px;
+    width: 225px;
+  }
+
+  .box-text {
+    display: inline-block;
+    font-size: 1.2rem;
+    margin-top: 3px;
+    width: 100%;
+  }
+</style>
---
+++ src/components/atoms/characterSheet/Portrait.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="portrait border">
+    <div class="box-input">
+      <img src="../../../../public/assets/melonlord.png" class="img-foo" alt="">
+    </div>
+    <span class="box-text center-caps">
+      Portrait
+    </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Portrait'
+  }
+</script>
+
+<style lang="less" scoped>
+  .portrait {
+    // background-color: white;
+    height: 212px;
+    padding: 10px;
+    width: 236px;
+  }
+
+  .box-input {
+    border: none;
+    padding: 5px;
+    height: 187px;
+    text-align: center;
+    width: 225px;
+  }
+
+  .img-foo {
+    padding-top: 10px;
+    height: 157px;
+    width: 195px;
+  }
+
+  .box-text {
+    display: inline-block;
+    font-size: 1.2rem;
+    margin-top: 3px;
+    width: 100%;
+  }
+</style>

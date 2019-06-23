6428fa1140c77bb1b14978d0c079d757cdef1001
Ecksi <jimecksi@gmail.com>
2019-06-22 21:23:55 -0600

    Refactor CharacterSheet

--- src/views/Learn.vue
+++ src/views/Learn.vue
@@ -6,94 +6,46 @@
         <PlayerName />
         <PlayerInfo />
       </header>
-
       <main class="bottom-block">
-        <section class="char-traits">
-          <!-- This can be an organism -->
-          <div class="attributes-skills">
-            <Attributes />
-            <div class="skillsauce">
-              <Bonus :type="'inspiration'"></Bonus>
-              <Bonus :type="'proficiency'"></Bonus>
-              <SavingThrows />
-              <Skills />
-            </div>
-          </div>
-
-          <div class="profsauce">
-          <!-- This can be an organism -->
-            <Bonus :type="'perception'"></Bonus>
-            <OtherProfs />
-          </div>
-        </section>
-
-
-        <section class="moo-cow char-traits">
-          <!-- This can be an organism -->
-          <div class="live-block">
-            <ArmorInitSpeed />
-            <HitPointBoxes />
-            <HitDiceDeathSaves />
-          </div>
-          <AttacksAndSpells />
-          <Equipment />
-        </section>
-
-        <section class="char-traits wizzle">
-          <!-- This can be an organism -->
-          <BackgroundTraits />
-          <RoundBox>Features & Traits</RoundBox>
-          <RoundBox>Portrait</RoundBox>
-        </section>
+        <ColumnOne />
+        <ColumnTwo />
+        <ColumnThree />
       </main>
     </div>
   </div>
 </template>
 
 <script>
-import ArmorInitSpeed from '@/components/molecules/characterSheet/ArmorInitSpeed';
-import AttacksAndSpells from '@/components/atoms/characterSheet/AttacksAndSpells';
-import Attributes from '@/components/molecules/characterSheet/Attributes';
-import BackgroundTraits from '@/components/molecules/characterSheet/BackgroundTraits';
-import Bonus from '@/components/atoms/characterSheet/Bonus';
-import Equipment from '@/components/molecules/characterSheet/Equipment';
-import HitDiceDeathSaves from '@/components/atoms/characterSheet/HitDiceDeathSaves';
-import HitPointBoxes from '@/components/molecules/characterSheet/HitPointBoxes';
-import OtherProfs from '@/components/atoms/characterSheet/OtherProfs';
+import ColumnOne from '@/components/organisms/characterSheet/ColumnOne';
+import ColumnTwo from '@/components/organisms/characterSheet/ColumnTwo';
+import ColumnThree from '@/components/organisms/characterSheet/ColumnThree';
 import PlayerInfo from '@/components/atoms/characterSheet/PlayerInfo';
 import PlayerName from '@/components/atoms/characterSheet/PlayerName';
-import RoundBox from '@/components/atoms/characterSheet/RoundBox';
-import SavingThrows from '@/components/molecules/characterSheet/SavingThrows';
-import Skills from '@/components/molecules/characterSheet/Skills';
 
 export default {
   name: 'Learn',
   components: {
-    ArmorInitSpeed,
-    AttacksAndSpells,
-    Attributes,
-    BackgroundTraits,
-    Bonus,
-    Equipment,
-    HitDiceDeathSaves,
-    HitPointBoxes,
-    OtherProfs,
+    ColumnOne,
+    ColumnTwo,
+    ColumnThree,
     PlayerInfo,
-    PlayerName,
-    RoundBox,
-    SavingThrows,
-    Skills,
+    PlayerName
   }
 };
 </script>
 
 <style lang="less" scoped>
   .paper {
+    background-image: url('../../public/assets/paper.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
     border: 1px #333 solid;
+    border-radius: 5px;
     display: flex;
     flex-direction: column;
     height: 1100px;
     margin: 0 auto;
+    margin-bottom: 20px;
     padding: 15px;
     // width: 800px;
     width: 840px;
@@ -106,45 +58,5 @@ export default {
   .bottom-block {
     display: flex;
     height: 90%;
-  }
-
-  .attributes-skills {
-    display: flex;
-    margin-bottom: 10px;
-  }
-
-  .skillsauce {
-    padding: 0 10px;
-  }
-
-  .profsauce > div:last-child {
-    margin-top: 10px;
-  }
-
-  .skillsauce > div:not(:last-child) {
-    margin-bottom: 10.75px;
-  }
-
-  .live-block {
-    background-color: #ddd;
-    border-radius: 10px;
-    padding: 15px 10px;
-    margin-bottom: 10px;
-  }
-
-  .moo-cow > div:last-child {
-    margin-top: 10px;
-  }
-
-  .moo-cow {
-    margin-right: 10px
-  }
-
-  .char-traits {
-    // width: 33%;
-  }
-
-  .wizzle > div:not(:last-child) {
-    margin-bottom: 10px;
   }
 </style>
